48463f0cd0e88254b83506e4fd73a4f87c4ac69e
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-05-25 13:37:06 -0500

    funcion recursiva

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const pathModules = requiere('path');
+const fsModules = requiere('fs');
+
+const absolutePath = process.argv[2];
+
+//Funcion recursiva
+const isFileOrDirectory = (absolutePath) => {
+    let arrayFileMd = [];
+    if (fsModules.lstatsync(absolutePath).isFile() && pathModules.extname(absolutePath).isMd()) {
+        arrayFileMd.push(absolutePath);
+        //console.log('es un archvo .md');
+    } else if (fsModules.lstatsync(absolutePath).isDirectory()) {
+        let contentArray = fsModules.readdirSync(absolutePath);
+        //console.log('contentArray');
+        contentArray.forEach((item) => {
+            const newRoute = pathModules.join(absolutePath, item);
+            arrayFileMd = arrayFileMd.concat(isFileOrDirectory(newRoute));
+        });
+    } else {
+        //console.log(arrayFileMd);
+    }
+   
+    return arrayFileMd;
+}
+ console.log(isFileOrDirectory(absolutePath));
